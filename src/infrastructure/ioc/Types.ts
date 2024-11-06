d8f391c266b130171a8fe6bec4d932a249ab0a0c
Maciej Stępień <maciej.stepien1994@gmail.com>
2024-11-06 13:31:33 +0100

    Update location helper, added all location to enum territory, removed unused method

--- src/infrastructure/ioc/Types.ts
+++ src/infrastructure/ioc/Types.ts
@@ -1,7 +1,6 @@
 const TYPES = {
   WeatherService: Symbol.for("WeatherService"),
   Logger: Symbol.for("Logger"),
-  Parser: Symbol.for("Parser"),
   HttpClient: Symbol.for("HttpClient"),
   WarningRepository: Symbol.for("WarningRepository"),
   LocationHelper: Symbol.for("LocationHelper"),
